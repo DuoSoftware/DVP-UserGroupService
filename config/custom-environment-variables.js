205c0284668d3efddc01f05f65fc3fe4c88c5973
coder a <duoacoder@gmail.com>
2020-02-02 10:24:29 +0530

    config setup

--- config/custom-environment-variables.js
+++ config/custom-environment-variables.js
@@ -1,36 +1,36 @@
-module.exports = {  
-    Security: {
-      ip: "SYS_REDIS_HOST",
-      port: "SYS_REDIS_PORT",
-      user: "SYS_REDIS_USER",
-      password: "SYS_REDIS_PASSWORD",
-      mode: "SYS_REDIS_MODE",
-      sentinels: {
-        hosts: "SYS_REDIS_SENTINEL_HOSTS",
-        port: "SYS_REDIS_SENTINEL_PORT",
-        name: "SYS_REDIS_SENTINEL_NAME"
-      }
-    },
-  
-    Mongo: {
-      ip: "SYS_MONGO_HOST",
-      port: "SYS_MONGO_PORT",
-      dbname: "SYS_MONGO_DB",
-      password: "SYS_MONGO_PASSWORD",
-      user: "SYS_MONGO_USER",
-      replicaset: "SYS_MONGO_REPLICASETNAME"
-    },
-  
-    Host: {
-      profilesearch: "EXTERNAL_PROFILE_SEARCH",
-      vdomain: "LB_FRONTEND",
-      domain: "HOST_NAME",
-      port: "HOST_USERSERVICE_PORT",
-      version: "HOST_VERSION"
-    },
-  
-    LBServer: {
-      ip: "LB_FRONTEND",
-      port: "LB_PORT"
+module.exports = {
+  Security: {
+    ip: "SYS_REDIS_HOST",
+    port: "SYS_REDIS_PORT",
+    user: "SYS_REDIS_USER",
+    password: "SYS_REDIS_PASSWORD",
+    mode: "SYS_REDIS_MODE",
+    sentinels: {
+      hosts: "SYS_REDIS_SENTINEL_HOSTS",
+      port: "SYS_REDIS_SENTINEL_PORT",
+      name: "SYS_REDIS_SENTINEL_NAME"
     }
-  };
+  },
+
+  Mongo: {
+    ip: "SYS_MONGO_HOST",
+    port: "SYS_MONGO_PORT",
+    dbname: "SYS_MONGO_DB",
+    password: "SYS_MONGO_PASSWORD",
+    user: "SYS_MONGO_USER",
+    replicaset: "SYS_MONGO_REPLICASETNAME"
+  },
+
+  Host: {
+    profilesearch: "EXTERNAL_PROFILE_SEARCH",
+    vdomain: "LB_FRONTEND",
+    domain: "HOST_NAME",
+    port: "HOST_USERSERVICE_PORT",
+    version: "HOST_VERSION"
+  },
+
+  LBServer: {
+    ip: "LB_FRONTEND",
+    port: "LB_PORT"
+  }
+};
--- config/default.js
+++ config/default.js
@@ -1,31 +1,31 @@
 module.exports = {
   Security: {
-    "mode":"instance",//instance, cluster, sentinel
-    "ip": "url",
-    "port": 6379,
-    "user": "User",
-    "password": "Password",
-    "sentinels":{
-      "hosts": "url",
-      "port":16389,
-      "name":"redis-cluster"
+    mode: "instance", //instance, cluster, sentinel
+    ip: "url",
+    port: 6379,
+    user: "User",
+    password: "Password",
+    sentinels: {
+      hosts: "url",
+      port: 16389,
+      name: "redis-cluster"
     }
   },
   Host: {
-    "profilesearch":"secondary",
-    "resource": "cluster",
-    "vdomain": "localhost",
-    "domain": "localhost",
-    "port": "3638",
-    "version": "1.0.0.0"
+    profilesearch: "secondary",
+    resource: "cluster",
+    vdomain: "localhost",
+    domain: "localhost",
+    port: "3638",
+    version: "1.0.0.0"
   },
   Mongo: {
-    "ip":"url",
-    "port":"27017",
-    "dbname":"dvpdb",
-    "password":"Password",
-    "user":"User",
-    "replicaset" :""
+    ip: "url",
+    port: "27017",
+    dbname: "dvpdb",
+    password: "Password",
+    user: "User",
+    replicaset: ""
   },
   LBServer: {
     ip: "LB_FRONTEND",
