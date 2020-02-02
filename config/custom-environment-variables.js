2a26389cfd368eb12ba78c448e02ec00e940d760
DuoB Coder <duobcoder@gmail.com>
2020-02-02 10:06:42 +0530

    Config Cleanup

--- config/custom-environment-variables.js
+++ config/custom-environment-variables.js
@@ -1,25 +1,4 @@
-module.exports = {
-    RabbitMQ: {
-      ip: "SYS_RABBITMQ_HOST",
-      port: "SYS_RABBITMQ_PORT",
-      user: "SYS_RABBITMQ_USER",
-      password: "SYS_RABBITMQ_PASSWORD",
-      vhost: "SYS_RABBITMQ_VHOST"
-    },
-  
-    Redis: {
-      mode: "SYS_REDIS_MODE",
-      ip: "SYS_REDIS_HOST",
-      port: "SYS_REDIS_PORT",
-      user: "SYS_REDIS_USER",
-      password: "SYS_REDIS_PASSWORD",
-      sentinels: {
-        hosts: "SYS_REDIS_SENTINEL_HOSTS",
-        port: "SYS_REDIS_SENTINEL_PORT",
-        name: "SYS_REDIS_SENTINEL_NAME"
-      }
-    },
-  
+module.exports = {  
     Security: {
       ip: "SYS_REDIS_HOST",
       port: "SYS_REDIS_PORT",
