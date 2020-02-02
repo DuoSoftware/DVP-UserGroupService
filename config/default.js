3ee2da6469aab238ef5651f13567c49b963c6601
DuoB Coder <duobcoder@gmail.com>
2020-02-02 09:06:36 +0530

    Config Changes

--- config/default.js
+++ config/default.js
@@ -1,85 +1,29 @@
 module.exports = {
-
-
-  MONGO_URI: process.env.MONGO_URI || 'MONGO_URL',
-  TOKEN_SECRET: process.env.TOKEN_SECRET || 'YOUR_UNIQUE_JWT_TOKEN_SECRET',
-
-  // OAuth 2.0
-  FACEBOOK_SECRET: process.env.FACEBOOK_SECRET || 'YOUR_FACEBOOK_CLIENT_SECRET',
-  FOURSQUARE_SECRET: process.env.FOURSQUARE_SECRET || 'YOUR_FOURSQUARE_CLIENT_SECRET',
-  GOOGLE_SECRET: process.env.GOOGLE_SECRET || 'YOUR_GOOGLE_CLIENT_SECRET',
-  //
-  GITHUB_SECRET: process.env.GITHUB_SECRET || 'YOUR_GITHUB_CLIENT_SECRET',
-  INSTAGRAM_SECRET: process.env.INSTAGRAM_SECRET || 'YOUR_INSTAGRAM_CLIENT_SECRET',
-  LINKEDIN_SECRET: process.env.LINKEDIN_SECRET || 'YOUR_LINKEDIN_CLIENT_SECRET',
-  TWITCH_SECRET: process.env.TWITCH_SECRET || 'YOUR_TWITCH_CLIENT_SECRET',
-  WINDOWS_LIVE_SECRET: process.env.WINDOWS_LIVE_SECRET || 'YOUR_MICROSOFT_CLIENT_SECRET',
-  YAHOO_SECRET: process.env.YAHOO_SECRET || 'YOUR_YAHOO_CLIENT_SECRET',
-  BITBUCKET_SECRET: process.env.BITBUCKET_SECRET || 'YOUR_BITBUCKET_CLIENT_SECRET',
-  SPOTIFY_SECRET: process.env.SPOTIFY_SECRET || 'YOUR_SPOTIFY_CLIENT_SECRET',
-
-  // OAuth 1.0
-  TWITTER_KEY: process.env.TWITTER_KEY || 'YOUR_TWITTER_CLIENT_KEY',
-  TWITTER_SECRET: process.env.TWITTER_SECRET || 'YOUR_TWITTER_CLIENT_SECRET',
-
-
-  "auth":{
-
-    // OAuth 2.0
-    FACEBOOK_SECRET: 'YOUR_FACEBOOK_CLIENT_SECRET',
-    FOURSQUARE_SECRET: 'YOUR_FOURSQUARE_CLIENT_SECRET',
-    GOOGLE_SECRET: 'YOUR_GOOGLE_CLIENT_SECRET',
-    //
-    GITHUB_SECRET: 'YOUR_GITHUB_CLIENT_SECRET',
-    INSTAGRAM_SECRET: 'YOUR_INSTAGRAM_CLIENT_SECRET',
-    LINKEDIN_SECRET: 'YOUR_LINKEDIN_CLIENT_SECRET',
-    TWITCH_SECRET: 'YOUR_TWITCH_CLIENT_SECRET',
-    WINDOWS_LIVE_SECRET: 'YOUR_MICROSOFT_CLIENT_SECRET',
-    YAHOO_SECRET: 'YOUR_YAHOO_CLIENT_SECRET',
-    BITBUCKET_SECRET:  'YOUR_BITBUCKET_CLIENT_SECRET',
-    SPOTIFY_SECRET: 'YOUR_SPOTIFY_CLIENT_SECRET',
-
-    // OAuth 1.0
-    TWITTER_KEY: 'YOUR_TWITTER_CLIENT_KEY',
-    TWITTER_SECRET: 'YOUR_TWITTER_CLIENT_SECRET',
-
-    login_verification: true,
-    signup_verification: false,
-    recaptcha_key: "YOUR_RECAPCHAKEY",
-
-    ui_host: 'http://localhost:3000/',
-    agent_host: 'http://localhost:3000/'
-
-  },
-  "Redis":
-  {
+  Redis: {
     "mode":"instance",//instance, cluster, sentinel
-    "ip": "REDIS_URL",
+    "ip": "ec2-18-189-192-150.us-east-2.compute.amazonaws.com",
     "port": 6379,
-    "user": "USER",
-    "password": "PASSWORD",
+    "user": "duo",
+    "password": "DuoS123",
     "sentinels":{
-      "hosts": "SENTNELS_URL",
+      "hosts": "138.197.90.92,45.55.205.92,138.197.90.92",
       "port":16389,
       "name":"redis-cluster"
     }
   },
-  "Security":
-  {
-
-    "ip" : "REDIS_URL",
-    "port": 6379,
-    "user": "USER",
-    "password": "PASSWORD",
+  Security: {
     "mode":"instance",//instance, cluster, sentinel
+    "ip": "ec2-18-189-192-150.us-east-2.compute.amazonaws.com",
+    "port": 6379,
+    "user": "duo",
+    "password": "DuoS123",
     "sentinels":{
-      "hosts": "SENTNELS_URL",
+      "hosts": "138.197.90.92,45.55.205.92,138.197.90.92",
       "port":16389,
       "name":"redis-cluster"
     }
   },
-  "Host":
-  {
+  Host: {
     "profilesearch":"secondary",
     "resource": "cluster",
     "vdomain": "localhost",
@@ -87,47 +31,23 @@ module.exports = {
     "port": "3638",
     "version": "1.0.0.0"
   },
-  "RabbitMQ":
-  {
-    "ip": "URL",
+  RabbitMQ: {
+    "ip": "ec2-18-189-192-150.us-east-2.compute.amazonaws.com",
     "port": 5672,
-    "user": "USER",
-    "password": "PASSWORD",
+    "user": "duo",
+    "password": "DuoS123",
     "vhost":'/'
   },
-  "Mongo":
-  {
-    "ip":"URL",
+  Mongo: {
+    "ip":"ec2-18-189-192-150.us-east-2.compute.amazonaws.com",
     "port":"27017",
-    "dbname":"USER",
-    "password":"PASSWORD",
-    "user":"USER",
+    "dbname":"dvpdb",
+    "password":"DuoS123",
+    "user":"duo",
     "replicaset" :""
   },
-  "Services" : {
-    "accessToken":"TOKEN",
-    "resourceServiceHost": "resourceservice.app.veery.cloud",
-    "resourceServicePort": "8831",
-    "resourceServiceVersion": "1.0.0.0",
-    "sipuserendpointserviceHost": "sipuserendpointservice.app.veery.cloud",
-    "sipuserendpointservicePort": "8831",
-    "sipuserendpointserviceVersion": "1.0.0.0",
-    "clusterconfigserviceHost": "clusterconfig.app.veery.cloud",
-    "clusterconfigservicePort": "8831",
-    "clusterconfigserviceVersion": "1.0.0.0",
-    "billingserviceHost": "billingservice.app.veery.cloud",
-    "billingservicePort": "4444",
-    "billingserviceVersion": "1.0.0.0",
-    "notificationServiceHost": "notificationservice.app1.veery.cloud",
-    "notificationServicePort": "8089",
-    "notificationServiceVersion": "1.0.0.0"
-  },
-
-  "Tenant": {
-    "activeTenant": 1
-  },
-
-  "ActiveDirectory": {
-    "groupName": "FaceTone"
+  LBServer: {
+    ip: "LB_FRONTEND",
+    port: "LB_PORT"
   }
 };
